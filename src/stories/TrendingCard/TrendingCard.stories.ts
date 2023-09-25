2e1b55f0fd07fe20492c1250a07659b07dc97e9a
Daniel Rojkind Vermeulen <drojkind@me.com>
2023-09-25 16:53:32 +0200

    add trending card story

---
+++ src/stories/TrendingCard/TrendingCard.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import TrendingCard from '.'
+
+const meta: Meta<typeof TrendingCard> = {
+  title: 'TrendingCard',
+  component: TrendingCard,
+  tags: ['autodocs'],
+  parameters: {
+    tags: ['autodocs'],
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    image: '/assets/images/trending/t1.jpeg',
+    imageAlt: '',
+    title: 'Soulection',
+    value: '135 ETH',
+    href: '#',
+    users: [
+      '/assets/images/users/u1.jpeg',
+      '/assets/images/users/u2.jpeg',
+      '/assets/images/users/u3.jpeg',
+      '/assets/images/users/u4.jpeg',
+    ],
+    userQuantity: 230,
+  },
+}
---
+++ src/stories/TrendingCard/index.tsx
@@ -0,0 +1,82 @@
+// Framework
+import Image from 'next/image'
+import Link from 'next/link'
+
+// Types
+interface CProps {
+  image: string
+  imageAlt: string
+  title: string
+  value: string
+  users: string[]
+  userQuantity: number
+  href: string
+}
+
+// Components
+import Paragraph from '@/stories/Paragraph'
+
+// Component Prep
+
+const UserImages = ({ children }: { children: string[] }) => {
+  return (
+    <div className="flex">
+      {children.map((user, index) => {
+        const marginLeft = `relative h-6 w-6 ${index > 0 ? '-ml-2' : ''}`
+        return (
+          <div key={user} className={marginLeft}>
+            <Image
+              src={user}
+              alt=""
+              width={24}
+              height={24}
+              className="h-full w-full overflow-hidden rounded-full object-cover"
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+/*--------------------------------------------------------------------*/
+
+/**
+ * Component
+ */
+
+const Heading = ({
+  image,
+  title,
+  value,
+  users,
+  userQuantity,
+  imageAlt,
+  href,
+}: CProps) => {
+  return (
+    <Link className="block w-max rounded-lg bg-white p-2" href={href} passHref>
+      <Image
+        className="mx-auto h-[224px] w-[240px] overflow-hidden rounded-lg"
+        src={image}
+        alt={imageAlt}
+        width={240}
+        height={224}
+      />
+      <div>
+        <div className="flex justify-between py-3">
+          <Paragraph as="p3">{title}</Paragraph>
+          <Paragraph as="p3">{value}</Paragraph>
+        </div>
+        <div className="flex justify-between">
+          <UserImages>{users}</UserImages>
+          <Paragraph as="p4" className="text-grey-dark">
+            + {userQuantity} others
+          </Paragraph>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default Heading
