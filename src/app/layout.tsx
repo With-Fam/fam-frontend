b770a211a30f39fc97e826e03f9b491758997672
Daniel Rojkind Vermeulen <drojkind@me.com>
2023-09-26 18:42:15 +0200

    CREATE footer layout section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import './global.scss'
 
 // Local Components
 import Navbar from '@/components/shared/Navbar'
+import Footer from '@/components/shared/Footer'
 
 /*--------------------------------------------------------------------*/
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className='font-abc'>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
