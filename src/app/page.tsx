961f1070529a14d5a98fa55008967aadb5779334
Daniel Rojkind Vermeulen <drojkind@me.com>
2023-09-26 08:47:32 +0200

    ADD favicon and homepage meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,28 @@ import AboveTheFold from '@/components/home/AboveTheFold'
  */
 
 export const metadata: Metadata = {
-  title: 'Page Title',
-  description: 'Page description',
-  keywords: [''],
+  title: 'Your fam, onchain',
+  description:
+    'Fam is a space for music communities to grow, collaborate and collectively fund creative projects',
+  keywords: [
+    'Music Community Platform',
+    'Collaborative Music Space',
+    'Collective Funding for Music Projects',
+    'Empowering Musicians',
+    'Community Growth and Engagement',
+    'Creative Project Funding',
+    'Music Collaboration Network',
+    'Support for Music Creators',
+    'Music Discovery and Promotion',
+    'Diverse Music Genres',
+    'Innovative Music Community',
+    'Music Community Building',
+  ],
   openGraph: {
     type: 'website',
     url: 'https://todo.dev',
     title: 'todo.dev',
-    description: 'Page description',
+    description: 'Fam is a space for music communities to grow, collaborate and collectively fund creative projects',
     siteName: 'todo.dev',
     images: [{ url: '/img/favicon.png', width: 128, height: 128 }],
   },
