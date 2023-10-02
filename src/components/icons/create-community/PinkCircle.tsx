549d299b3e4e868f217a68c07c5d6db2ceb6ded1
Regan Langford <regan.reihana@gmail.com>
2023-10-02 04:01:14 +0000

    REMOVE returns on dumb/logicless components

--- src/components/icons/create-community/PinkCircle.tsx
+++ src/components/icons/create-community/PinkCircle.tsx
@@ -9,34 +9,32 @@ interface PinkCircleProps {
  * Component
  */
 
-const PinkCircle = ({ className }: PinkCircleProps): JSX.Element => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      className={className}
-    >
-      <ellipse
-        cx="24"
-        cy="24"
-        rx="18"
-        ry="18"
-        transform="rotate(90 24 24)"
-        fill="#FDA4FF"
-      />
-      <ellipse
-        cx="23.9995"
-        cy="24.0001"
-        rx="5.01031"
-        ry="5.01031"
-        transform="rotate(90 23.9995 24.0001)"
-        fill="white"
-      />
-    </svg>
-  )
-}
+const PinkCircle = ({ className }: PinkCircleProps): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    className={className}
+  >
+    <ellipse
+      cx="24"
+      cy="24"
+      rx="18"
+      ry="18"
+      transform="rotate(90 24 24)"
+      fill="#FDA4FF"
+    />
+    <ellipse
+      cx="23.9995"
+      cy="24.0001"
+      rx="5.01031"
+      ry="5.01031"
+      transform="rotate(90 23.9995 24.0001)"
+      fill="white"
+    />
+  </svg>
+)
 
 export default PinkCircle
--- src/components/icons/create-community/YellowPolygon.tsx
+++ src/components/icons/create-community/YellowPolygon.tsx
@@ -9,22 +9,20 @@ interface YellowPolygonProps {
  * Component
  */
 
-const YellowPolygon = ({ className }: YellowPolygonProps): JSX.Element => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      className={className}
-    >
-      <path
-        d="M8 20.954L24.1906 6L40.3812 20.954V42.1032H8V20.954Z"
-        fill="#F3D742"
-      />
-    </svg>
-  )
-}
+const YellowPolygon = ({ className }: YellowPolygonProps): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    className={className}
+  >
+    <path
+      d="M8 20.954L24.1906 6L40.3812 20.954V42.1032H8V20.954Z"
+      fill="#F3D742"
+    />
+  </svg>
+)
 
 export default YellowPolygon
