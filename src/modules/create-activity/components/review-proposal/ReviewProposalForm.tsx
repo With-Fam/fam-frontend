aaab43a2395b69317a036f4316c73e9073df724e
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-27 19:15:20 -0300

    I remove unused imports.

--- src/modules/create-activity/components/review-proposal/ReviewProposalForm.tsx
+++ src/modules/create-activity/components/review-proposal/ReviewProposalForm.tsx
@@ -3,7 +3,6 @@
 // Framework
 import { MutableRefObject, useCallback } from 'react'
 import dynamic from 'next/dynamic'
-import { Interface, parseEther } from 'ethers'
 
 // Third Parties
 import toast from 'react-hot-toast'
@@ -34,12 +33,9 @@ import TitleInput from '@/modules/create-activity/components/review-proposal/Tit
 import { AddButton } from './AddButton'
 
 // Helpers
-import { governorAbi, tokenAbi } from '@/data/contract/abis'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useDaoStore } from '@/modules/dao'
 import { useChainStore } from '@/utils/stores/useChainStore'
-import { useProposalStore } from '@/modules/create-activity/stores'
-import { useCheckAuth } from '@/hooks/useCheckAuth'
 import { partyAbi } from '@/data/contract/abis/Party'
 
 /*--------------------------------------------------------------------*/
@@ -51,7 +47,6 @@ export function ReviewProposalForm({
   setLoading,
   setLoadingMessage,
 }: ReviewProposalFormProps): JSX.Element {
-  const { address: connectedWallet } = useAccount()
   const methods = useForm<ReviewProposalFormValues>({
     resolver: yupResolver(schema),
     defaultValues,
@@ -59,96 +54,58 @@ export function ReviewProposalForm({
   const { handleSubmit } = methods
   const addresses = useDaoStore((state) => state.addresses)
   const chain = useChainStore((x) => x.chain)
-  const {
-    wagmiData: { address },
-  } = useCheckAuth()
-  const { transactions } = useProposalStore()
 
-  const { data: votes } = useContractRead({
-    address: addresses?.token as AddressType,
-    abi: tokenAbi,
-    enabled: !!address,
-    functionName: 'getVotes',
-    chainId: chain.id,
-    args: [address as AddressType],
-  })
+  const onSubmit = useCallback(async () => {
+    setLoading(true)
+    try {
+      const latestSnapIndex = 0n
+      const currentDate = new Date()
+      const oneMonthLater = new Date(
+        currentDate.setMonth(currentDate.getMonth() + 1)
+      )
+      const maxExecutableTime = Math.floor(oneMonthLater.getTime() / 1000)
+      const hardCodedTransferProposal =
+        '0x00000004000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000020000000000000000000000000cfbf34d385ea2d5eb947063b67ea226dcda3dc3800000000000000000000000000000000000000000000000000005af3107a400000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
 
-  const { data: proposalThreshold } = useContractRead({
-    address: addresses?.governor as AddressType,
-    chainId: chain.id,
-    abi: governorAbi,
-    functionName: 'proposalThreshold',
-  })
-
-  const onSubmit = useCallback(
-    async (values: ReviewProposalFormValues) => {
-      setLoading(true)
-      try {
-        console.log('SWEETS onSubmit')
-
-        const latestSnapIndex = 9236006n
-        console.log('SWEETS latestSnapIndex', latestSnapIndex)
-
-        const ABI = ['function transfer(address to, uint amount)']
-        console.log('SWEETS ABI', ABI)
-
-        const iface = new Interface(ABI)
-        console.log('SWEETS iface', iface)
-
-        const encoded = iface.encodeFunctionData('transfer', [
-          connectedWallet,
-          parseEther('0.001'),
-        ])
-        console.log('SWEETS encoded', encoded)
-        const currentDate = new Date()
-        const oneMonthLater = new Date(
-          currentDate.setMonth(currentDate.getMonth() + 1)
-        )
-        const maxExecutableTime = Math.floor(oneMonthLater.getTime() / 1000)
-        console.log('SWEETS maxExecutableTime:', maxExecutableTime)
-
-        const proposal = {
-          maxExecutableTime,
-          cancelDelay: '0',
-          proposalData:
-            '0x00000004000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000020000000000000000000000000cfbf34d385ea2d5eb947063b67ea226dcda3dc3800000000000000000000000000000000000000000000000000005af3107a400000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-        }
-        const args = [proposal, latestSnapIndex] as any
-        const config = await prepareWriteContract({
-          abi: partyAbi,
-          functionName: 'propose',
-          address: community as AddressType,
-          chainId: chain.id,
-          args,
-        })
-
-        const response = await writeContract(config)
-        await waitForTransaction({ hash: response.hash })
-        setLoadingMessage('Proposal posted. Redirecting...')
-      } catch (err: any) {
-        setLoading(false)
-        console.error(err)
-
-        if (err.name === 'ConnectorNotFoundError') {
-          toast.error(ERROR_CODE.CONNECTOR_NOT_FOUND)
-          return
-        }
-
-        if (err.shortMessage === 'User rejected the request.') {
-          toast.error(ERROR_CODE.REJECTED)
-          console.log('err::', err.shortMessage)
-          return
-        }
-        if (err.code === 'ACTION_REJECTED') {
-          console.log('err::', err.code)
-          toast.error(ERROR_CODE.REJECTED)
-          return
-        }
-        toast.error(err.messageD)
+      const proposal = {
+        maxExecutableTime,
+        cancelDelay: '0',
+        proposalData: hardCodedTransferProposal,
       }
-    },
-    [addresses, proposalThreshold, votes, chain.id, transactions]
-  )
+      console.log('SWEETS proposal:', proposal)
+
+      const args = [proposal, latestSnapIndex] as any
+      const config = await prepareWriteContract({
+        abi: partyAbi,
+        functionName: 'propose',
+        address: community as AddressType,
+        chainId: chain.id,
+        args,
+      })
+
+      const response = await writeContract(config)
+      await waitForTransaction({ hash: response.hash })
+      setLoadingMessage('Proposal posted. Redirecting...')
+    } catch (err: any) {
+      setLoading(false)
+      console.error(err)
+
+      if (err.name === 'ConnectorNotFoundError') {
+        toast.error(ERROR_CODE.CONNECTOR_NOT_FOUND)
+        return
+      }
+
+      if (err.shortMessage === 'User rejected the request.') {
+        toast.error(ERROR_CODE.REJECTED)
+        return
+      }
+      if (err.code === 'ACTION_REJECTED') {
+        toast.error(ERROR_CODE.REJECTED)
+        return
+      }
+      toast.error(err.messageD)
+    }
+  }, [addresses, chain.id])
 
   return (
     <FormProvider {...methods}>
