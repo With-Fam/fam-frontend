f56af8770b5d91e195feedf0907eb731489cb6a3
SeniorDev <tech.eng.me@gmail.com>
2024-05-30 06:33:21 -0400

    Revert

--- src/components/community/CommunityMembers/Member.tsx
+++ src/components/community/CommunityMembers/Member.tsx
@@ -13,10 +13,9 @@ const UserAvatar = dynamic(() => import('@/components/shared/UserAvatar'), {
 
 const Member = ({ address }: { address: Address }) => {
   const { profileName, pfpImage } = useEnsProfile(address)
-  const isFounder = true
 
   return (
-    <section className="flex items-center gap-2">
+    <div className="mb-4 flex items-center gap-2 sm:mb-0">
       {pfpImage ? (
         <div className="relative !h-8 !w-8 overflow-hidden rounded-full">
           <Image src={pfpImage} alt="" layout="fill" />
@@ -34,12 +33,7 @@ const Member = ({ address }: { address: Address }) => {
           address={address}
         />
       )}
-      {isFounder && (
-        <div className="flex h-8 w-16 items-center justify-center rounded-full bg-orange-secondary">
-          <p className="font-abc text-[12px] text-orange">Founder</p>
-        </div>
-      )}
-    </section>
+    </div>
   )
 }
 
