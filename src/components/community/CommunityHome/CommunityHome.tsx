e9cc2c10cdadc2b30ba9d822f5cdf18719cffd4a
SeniorDev <tech.eng.me@gmail.com>
2024-05-30 08:52:11 -0400

    I add joined section

--- src/components/community/CommunityHome/CommunityHome.tsx
+++ src/components/community/CommunityHome/CommunityHome.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import Joined from './Joined'
 import General from './General'
 
 const CommunityHome = () => {
   return (
-    <main className="relative mx-auto max-w-[936px]">
+    <main className="relative mx-auto max-w-[936px] px-2">
       <General />
+      <Joined />
     </main>
   )
 }
