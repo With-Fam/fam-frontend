68f79d065991b94111a90694d9882a5599f54a9e
SeniorDev <tech.eng.me@gmail.com>
2024-06-05 11:55:58 -0400

    I fix feedback

--- src/modules/create-community/components/confirm/ConfirmForm.tsx
+++ src/modules/create-community/components/confirm/ConfirmForm.tsx
@@ -6,23 +6,19 @@ import { FormProvider, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import toast from 'react-hot-toast'
 import schema, { type ConfirmFormValues } from './schema'
-import { useFormStore } from '@/modules/create-community'
-import ConfirmDropDown from './ConfirmDropDown'
-import ConfirmItem from './ConfirmItem'
-import ConfirmTitle from './ConfirmTitle'
 import ConfirmCheckbox from './ConfirmCheckbox'
 import ContinueButton from '@/modules/ContinueButton'
-import { IPFSImage } from '@/components/ipfs/IPFSImage'
 import useDeploy from '@/hooks/useDeploy'
 import useConnectedWallet from '@/hooks/useConnectedWallet'
 import { CHAIN_ID } from '@/constants/defaultChains'
 import SwitchNetworkButton from '@/components/SwitchNetworkButton'
+import MemebershipConfirmation from '@/modules/create-community/components/confirm/MembershipConfirmation'
+import ProfileConfirmation from '@/modules/create-community/components/confirm/ProfileConfirmation'
 
 export function ConfirmForm(): JSX.Element {
   const { wallet } = useConnectedWallet()
   const walletChainId = parseInt(wallet?.chainId.split(':')[1] as string, 10)
   const isCorrectChain = walletChainId === CHAIN_ID
-  const { general, membership } = useFormStore()
   const {
     isLoading,
     handleDeploy,
@@ -59,51 +55,11 @@ export function ConfirmForm(): JSX.Element {
         changed later via a community vote.
       </p>
       <form onSubmit={handleSubmit(handleDeploy)}>
-        <ConfirmDropDown text="Profile">
-          <div className="px-4 py-6 ">
-            <div>
-              <ConfirmTitle>DAO AVATAR</ConfirmTitle>
-              <div className="mt-2">
-                <IPFSImage
-                  src={general.daoAvatar as any}
-                  alt="dao avatar"
-                  width={80}
-                  height={80}
-                  className="h-20 w-20 rounded-full"
-                />
-              </div>
-            </div>
-            <ConfirmItem label="DAO NAME">{general.daoName}</ConfirmItem>
-            <ConfirmItem label="DAO SYMBOL">{general.daoSymbol}</ConfirmItem>
-            <ConfirmItem label="DAO WEBSITE">{general.daoWebsite}</ConfirmItem>
-          </div>
-        </ConfirmDropDown>
-        <ConfirmDropDown text="Memberships">
-          <div className="px-4 py-6 ">
-            <ConfirmItem label="Membership price">
-              {membership.membershipPrice} ETH
-            </ConfirmItem>
-            <ConfirmItem label="Mint Period">
-              {membership.mintPeriod} Days
-            </ConfirmItem>
-            <ConfirmItem label="Revenue Split">
-              {membership.revenueSplit}%
-            </ConfirmItem>
-            <div className="mt-2">
-              <ConfirmTitle>{`FOUNDER(S)`}</ConfirmTitle>
-              <div className="mt-2 space-y-2">
-                {membership.founders.map((founder, i) => (
-                  <p key={`${founder.founderAddress}-${i}`}>
-                    {founder.founderAddress}
-                  </p>
-                ))}
-              </div>
-            </div>
-          </div>
-        </ConfirmDropDown>
+        <ProfileConfirmation />
+        <MemebershipConfirmation />
         <div className="mt-10">
           <ConfirmCheckbox name="deployDaoAcceptance">
-            I have acknolwedged and agree to the{' '}
+            I have acknowlwedged and agree to the{' '}
             <Link href="#" className="text-orange">
               Fam Terms of Service
             </Link>
