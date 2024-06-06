05eb1ae192ed8b8464a09130f0ece89727bdeb50
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-06 16:33:46 -0500

    I remove dev logging.

--- src/hooks/useCreateParty.tsx
+++ src/hooks/useCreateParty.tsx
@@ -1,95 +1,20 @@
 import {
-  ATOMIC_MANUAL_PARTY,
   CROWDFUND_PARTY_FACTORY,
   INITIAL_ETH_CROWDFUND,
-  PARTY_IMPLEMENTATION,
 } from '@/constants/addresses'
-import { atomicManualPartyAbi } from '@/data/contract/abis/AtomicManualParty'
 import usePrivyWalletClient from '@/hooks/usePrivyWalletClient'
 import { useFormStore } from '@/modules/create-community'
-import { AddressType } from '@/types'
 import { getPublicClient } from '@/utils/viem'
 import getViemNetwork from '@/utils/viem/getViemNetwork'
-import { ZeroAddress } from 'ethers'
-import { useAccount } from 'wagmi'
 import { CHAIN_ID } from '@/constants/defaultChains'
-import useConnectedWallet from '@/hooks/useConnectedWallet'
 import { crowdfundFactoryAbi } from '@/data/contract/abis/CrowdfundFactory'
-import { zeroAddress } from 'viem'
 
 const useCreateParty = () => {
   const chainId = CHAIN_ID
   const { auctionSettings } = useFormStore()
-  const { connectedWallet: address } = useConnectedWallet()
   const { walletClient } = usePrivyWalletClient()
 
-  const createParty = async () => {
-    if (!walletClient) return { error: 'Wallet client not found' }
-    const totalVotingPower = 100000000000000000000n
-    const passThreshold =
-      ((auctionSettings.proposalThreshold / 100) * Number(totalVotingPower)) /
-      1e18
-    const BPS_MULTIPLIER = 100
-    const passThresholdBps = passThreshold * BPS_MULTIPLIER
-
-    try {
-      const ONE_HOUR = 60 * 60
-      const MINIMUM_VOTE_DURATION = ONE_HOUR
-      const partyMemberVotingPowers = [1000000n]
-      const partyMembers = [address as AddressType]
-      const publicClient = getPublicClient(chainId)
-      const contractConfig = {
-        address: ATOMIC_MANUAL_PARTY[chainId],
-        chain: getViemNetwork(chainId),
-        abi: atomicManualPartyAbi,
-        functionName: 'createParty',
-        args: [
-          PARTY_IMPLEMENTATION[chainId],
-          {
-            governance: {
-              hosts: [address as AddressType],
-              voteDuration: MINIMUM_VOTE_DURATION,
-              executionDelay: auctionSettings.executionDelay * ONE_HOUR,
-              passThresholdBps,
-              totalVotingPower,
-              feeBps: 0,
-              feeRecipient: ZeroAddress as AddressType,
-            },
-            proposalEngine: {
-              enableAddAuthorityProposal: true,
-              allowArbCallsToSpendPartyEth: true,
-              allowOperators: true,
-              distributionsConfig: 1,
-            },
-            name: 'PARTY',
-            symbol: 'FAM',
-            customizationPresetId: 0n,
-          },
-          [],
-          [],
-          1715603725,
-          partyMembers,
-          partyMemberVotingPowers,
-          partyMembers,
-        ],
-      } as any
-      const { request } = await publicClient.simulateContract(contractConfig)
-      const txHash = await walletClient.writeContract(request as any)
-
-      let transaction
-      if (txHash) {
-        transaction = await publicClient.waitForTransactionReceipt({
-          hash: txHash,
-        })
-      }
-      return transaction
-    } catch (error) {
-      return { error }
-    }
-  }
-
   const createInitialETHCrowdfund = async () => {
-    console.log('SWEETS createInitialETHCrowdfund')
     if (!walletClient) return { error: 'Wallet client not found' }
     const totalVotingPower = 100000000000000000000n
     const passThreshold =
@@ -117,8 +42,6 @@ const useCreateParty = () => {
         minContribution: 1000000000000000n,
         minTotalContributions: 1000000000000000n,
       }
-      console.log('SWEETS crowdfundOpts', crowdfundOpts)
-
       const governanceOpts = {
         executionDelay: 604800,
         feeBps: 250,
@@ -126,8 +49,8 @@ const useCreateParty = () => {
         hosts: ['0xb5acDED340D66678f01097818940A0F028DAFB8d'],
         partyFactory: '0xB418f5B001Af94A91daB2cE641E39722e1d9dDAC',
         partyImpl: '0xeFA4054F3Db3D1f5e981513a3d8A33D91FC97dc1',
-        passThresholdBps: 3000,
-        voteDuration: 604800,
+        passThresholdBps: passThresholdBps,
+        voteDuration: MINIMUM_VOTE_DURATION,
       }
       const proposalEngineOpts = {
         allowArbCallsToSpendPartyEth: true,
@@ -163,8 +86,6 @@ const useCreateParty = () => {
           createGateCallData,
         ],
       } as any
-      console.log('SWEETS contractConfig', contractConfig)
-
       const { request } = await publicClient.simulateContract(contractConfig)
       const txHash = await walletClient.writeContract(request as any)
 
@@ -176,12 +97,11 @@ const useCreateParty = () => {
       }
       return transaction
     } catch (error) {
-      console.log('SWEETS createInitialETHCrowdfund errror', error)
       return { error }
     }
   }
 
-  return { createParty, createInitialETHCrowdfund }
+  return { createInitialETHCrowdfund }
 }
 
 export default useCreateParty
