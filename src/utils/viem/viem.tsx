bd4ca1922f19089ff33c1ae2c44a6c25eb225771
SeniorDev <tech.eng.me@gmail.com>
2024-06-12 15:59:43 -0400

    I fix feedback

--- src/utils/viem/viem.tsx
+++ src/utils/viem/viem.tsx
@@ -1,13 +1,11 @@
 import { Chain, PublicClient, createPublicClient, http } from 'viem'
 import getViemNetwork from './getViemNetwork'
-import { mapChainIdToEndpoint } from '@/utils/alchemy/mapChainIdToEndpoint'
-import { CHAIN_ID } from '@/constants/defaultChains'
 
 export const getPublicClient = (chainId: number) => {
   const chain = getViemNetwork(chainId)
   const publicClient = createPublicClient({
     chain: chain as Chain,
-    transport: http(mapChainIdToEndpoint(CHAIN_ID)),
+    transport: http(),
   }) as PublicClient
   return publicClient
 }
