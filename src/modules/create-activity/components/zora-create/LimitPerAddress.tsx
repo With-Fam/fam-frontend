c4d4dd5008db57fc6a7b2215a06690c076d31264
SeniorDev <tech.eng.me@gmail.com>
2024-06-17 10:16:28 -0400

    I fix feedback

--- src/modules/create-activity/components/zora-create/LimitPerAddress.tsx
+++ src/modules/create-activity/components/zora-create/LimitPerAddress.tsx
@@ -1,5 +1,5 @@
 import SelectButton from '@/modules/create-activity/components/zora-create/SelectButton'
-import { EDITON_SIZE, useProposalStore } from '@/modules/create-activity/stores'
+import { LIMIT, useProposalStore } from '@/modules/create-activity/stores'
 
 const LimitPerAddress = () => {
   const { limitPerAddress, setLimitPerAddress } = useProposalStore()
@@ -7,18 +7,18 @@ const LimitPerAddress = () => {
   return (
     <section className="mt-4 rounded-md bg-white pb-3">
       <div className="flex items-center gap-1 px-4 pt-4 text-left font-abcMedium text-sm">
-        Edition size{' '}
+        Limit per address{' '}
       </div>
       <div className="mt-4 flex grid w-full grid-cols-2 gap-2 px-4 md:gap-8">
         <SelectButton
-          isActive={limitPerAddress === EDITON_SIZE.OPEN}
+          isActive={limitPerAddress === LIMIT.UNLIMITED}
           label="Unlimited"
-          onClick={() => setLimitPerAddress(EDITON_SIZE.OPEN)}
+          onClick={() => setLimitPerAddress(LIMIT.UNLIMITED)}
         />
         <SelectButton
-          isActive={limitPerAddress === EDITON_SIZE.ONEOFONE}
+          isActive={limitPerAddress === LIMIT.CUSTOM}
           label="Custom"
-          onClick={() => setLimitPerAddress(EDITON_SIZE.ONEOFONE)}
+          onClick={() => setLimitPerAddress(LIMIT.CUSTOM)}
         />
       </div>
     </section>
