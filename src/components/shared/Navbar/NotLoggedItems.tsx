2415b65c7dbffdd68394d1efb30c863adb272126
SeniorDev <tech.eng.me@gmail.com>
2024-06-19 16:06:30 -0400

    I fix it

--- src/components/shared/Navbar/NotLoggedItems.tsx
+++ src/components/shared/Navbar/NotLoggedItems.tsx
@@ -4,11 +4,8 @@ import Paragraph from '@/stories/Paragraph'
 const NotLoggedItems = (): JSX.Element => {
   const { login } = usePrivy()
 
-  const connect = async () => {
-    login()
-  }
   return (
-    <button onClick={connect} className="pointer-events-auto">
+    <button onClick={login} className="pointer-events-auto">
       <Paragraph
         className="rounded-3xl bg-black px-4 py-2 text-white sm:px-6 sm:py-2.5"
         as="p2"
