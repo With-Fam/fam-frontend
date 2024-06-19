232f0c14ef3c8593e05f2305dbe8fc29a7e1a5f7
SeniorDev <tech.eng.me@gmail.com>
2024-06-19 14:54:20 -0400

    I fix feedback

--- src/app/(main)/community/[network]/[community]/[proposalId]/page.tsx
+++ src/app/(main)/community/[network]/[community]/[proposalId]/page.tsx
@@ -11,6 +11,7 @@ import EnsAddress from '@/components/shared/EnsAddress'
 import { useProposalProvider } from '@/contexts/ProposalProvider'
 import useConnectedWallet from '@/hooks/useConnectedWallet'
 import { PROPOSAL_STATUS } from '@/hooks/useProposalData'
+import useProposalTimer from '@/hooks/useProposalTimer'
 import getDiffFormattedDuration from '@/utils/getDiffFormattedDuration'
 import getProposalStatus from '@/utils/getProposalStatus'
 import { usePrivy } from '@privy-io/react-auth'
@@ -31,28 +32,7 @@ export default function CommunityProposal(): JSX.Element {
   const status = getProposalStatus(proposal)
   const { push } = useRouter()
   const { community, network } = useParams()
-
-  const [countdown, setCountdown] = useState('')
-
-  useEffect(() => {
-    if (!proposal) return
-    const timer = setInterval(() => {
-      if (Date.now() >= proposal.maxExecutableTime * 1000) {
-        clearInterval(timer)
-        setCountdown('Finished')
-        return
-      }
-      const diff = getDiffFormattedDuration(
-        Date.now(),
-        new Date(proposal.maxExecutableTime * 1000)
-      )
-      setCountdown(diff)
-    }, 1000)
-
-    return () => {
-      clearInterval(timer)
-    }
-  }, [proposal])
+  const { countdown } = useProposalTimer(proposal)
 
   return (
     <main className="relative mx-auto mt-8 max-w-[936px] px-2 pb-4">
