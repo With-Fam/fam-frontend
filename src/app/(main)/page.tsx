60e9f329970518077249fe72994e9251fd982888
Nick Smith <nick.cam.smith@gmail.com>
2024-07-04 15:43:30 +1000

    Fix opengraph issues

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,25 +1,24 @@
-// Framework
+// framework
 import type { Metadata } from 'next'
 
-// Local Component
-import {
-  CollectiveCulture,
-  FAQ,
-  FeatureWidgets,
-  Hero,
-  TrendingSection,
-} from '@/components/home'
-import { Suspense } from 'react'
+// Types
+import type { PropsWithChildren } from 'react'
+
+// Styles
+import './global.scss'
+
+// Local Components
+import { Navbar, Footer } from '@/components/shared'
 
 /*--------------------------------------------------------------------*/
 
 /**
- * Page
+ * Component
  */
 
-export const metadata = {
-  metadataBase: new URL('https://withfam.xyz'),
-  title: 'Fam',
+export const metadata: Metadata = {
+  metadataBase: new URL('https://app.withfam.xyz'),
+  title: 'Your fam, onchain',
   description:
     'Fam is a space for music communities to grow, collaborate and collectively fund creative projects',
   keywords: [
@@ -37,46 +36,24 @@ export const metadata = {
     'Music Community Building',
   ],
   openGraph: {
-    url: 'https://withfam.xyz',
+    url: 'https://app.withfam.xyz',
     title: 'Your fam, onchain',
     description:
       'Fam is a space for music communities to grow, collaborate and collectively fund creative projects',
-    images: [
-      {
-        url: 'https://withfam.xyz/opengraph-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Open Graph Image',
-      },
-    ],
+    images: ['/opengraph-image.png'],
   },
-};
+}
 
-const Home = (): JSX.Element => {
+export default function RootLayout({
+  children,
+}: PropsWithChildren): JSX.Element {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords.join(', ')} />
-
-        {/* Open Graph Metadata */}
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
-        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
-        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
-      </Head>
-      <Suspense>
-      <Hero />
-      <CollectiveCulture />
-      <FeatureWidgets />
-      <FAQ />
-    </Suspense>
-    </>
-  );
-};
-
-export default Home;
+    <html lang="en">
+      <body className="flex min-h-screen flex-col bg-background font-abc">
+        <Navbar logged={false} />
+        <main className="grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
