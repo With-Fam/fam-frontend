f37ad23d0018ce282bf86ac2295940573a61aefe
Nick Smith <nick.cam.smith@gmail.com>
2024-07-04 15:51:56 +1000

    Fix opengraph issues

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,20 @@
-// framework
+// Framework
 import type { Metadata } from 'next'
 
-// Types
-import type { PropsWithChildren } from 'react'
-
-// Styles
-import './global.scss'
-
-// Local Components
-import { Navbar, Footer } from '@/components/shared'
+// Local Component
+import {
+  CollectiveCulture,
+  FAQ,
+  FeatureWidgets,
+  Hero,
+  TrendingSection,
+} from '@/components/home'
+import { Suspense } from 'react'
 
 /*--------------------------------------------------------------------*/
 
 /**
- * Component
+ * Page
  */
 
 export const metadata: Metadata = {
@@ -44,16 +45,15 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({
-  children,
-}: PropsWithChildren): JSX.Element {
+const Home = (): JSX.Element => {
   return (
-    <html lang="en">
-      <body className="flex min-h-screen flex-col bg-background font-abc">
-        <Navbar logged={false} />
-        <main className="grow">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <Suspense>
+      <Hero />
+      <CollectiveCulture />
+      <FeatureWidgets />
+      <FAQ />
+    </Suspense>
   )
 }
+
+export default Home
