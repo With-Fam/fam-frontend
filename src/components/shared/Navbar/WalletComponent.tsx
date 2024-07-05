eda7ffddac00f0e4d54abc2b9e865afffff17341
SeniorDev <tech.eng.me@gmail.com>
2024-07-05 12:33:40 -0400

    I fix feedback

--- src/components/shared/Navbar/WalletComponent.tsx
+++ src/components/shared/Navbar/WalletComponent.tsx
@@ -9,7 +9,7 @@ type WalletComponentProps = {
 const WalletComponent = ({
   userBalance,
 }: WalletComponentProps): JSX.Element => (
-  <div className="flex items-center justify-between gap-2 rounded-xl bg-background">
+  <div className="flex items-center justify-between gap-1 rounded-xl">
     <ChainIcon chainId={CHAIN_ID} />
     <Paragraph as="p3" className="text-black">
       {Number(userBalance).toFixed(2)} ETH
