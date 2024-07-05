4057351bc85e696ed37a5123b5ab5879439cc507
Nick Smith <nick.cam.smith@gmail.com>
2024-07-05 14:53:22 +1000

    Revert ZoraProposal component to original

--- src/components/Pages/CommunityPage/ProposalPage/ZoraProposal.tsx
+++ src/components/Pages/CommunityPage/ProposalPage/ZoraProposal.tsx
@@ -1,32 +1,15 @@
 import getIpfsLink from '@/lib/getIpfsLink'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
 
 const ZoraProposal = ({ info }: any) => {
-  const [imgSrc, setImgSrc] = useState(getIpfsLink(info.collectionImage))
-  const [isError, setIsError] = useState(false)
-
-  useEffect(() => {
-    setImgSrc(getIpfsLink(info.collectionImage))
-    setIsError(false)
-  }, [info.collectionImage])
-
-  const handleError = () => {
-    if (!isError) {
-      setImgSrc('/fallback-image.png')
-      setIsError(true)
-    }
-  }
-
   return (
     <div className="flex w-fit items-center gap-2 rounded-md border p-4">
       <Image
-        src={imgSrc}
+        src={getIpfsLink(info.collectionImage)}
         width={64}
         height={64}
         className="overflow-hidden rounded-md"
-        alt={info.title}
-        onError={handleError}
+        alt=""
       />
       <div className="space-y-1">
         <p className="font-abcMedium text-[20px]">{info.title}</p>
