631a68dd7e34a5b9e397db4686406b700b1e6c8f
SeniorDev <tech.eng.me@gmail.com>
2024-07-12 03:43:28 -0400

    WIP

--- src/components/Pages/CommunityPage/ProposalPage/ZoraProposal.tsx
+++ src/components/Pages/CommunityPage/ProposalPage/ZoraProposal.tsx
@@ -14,7 +14,6 @@ const ZoraProposal = ({ info }: any) => {
       <div className="space-y-1">
         <p className="font-abcMedium text-[20px]">{info.title}</p>
         <p className="font-abc text-[16px]">{info.collectionName}</p>
-        <p className="font-abc text-[14px] text-grey">{info.collectionDesc}</p>
       </div>
     </div>
   )
