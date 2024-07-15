6338219a9ea4d22be3b1d9bfe556e1b65a098bc5
SeniorDev <tech.eng.me@gmail.com>
2024-07-15 05:57:31 -0400

    I fix feedback

---
+++ src/components/Pages/CommunityPage/ProposalPage/VotedLabel.tsx
@@ -0,0 +1,12 @@
+import { Icon } from '@/components/Icon'
+
+const VotedLabel = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <Icon id="check" fill="#45D039" />
+      Voted
+    </div>
+  )
+}
+
+export default VotedLabel
