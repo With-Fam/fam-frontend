fb11b90c6c2de32cb75422765999d6b0c092e873
SeniorDev <tech.eng.me@gmail.com>
2024-07-16 10:23:56 -0400

    I remove unnecessary metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,9 @@ import '@/styles/quill-default.scss'
 import '@/styles/quill-editor.scss'
 import '@/styles/quill-toolbar.scss'
 
-import type { Metadata } from 'next'
 import { WalletContext } from '@/contexts/WalletContext'
 import type { PropsWithChildren } from 'react'
 import { ProgressBar } from '@/components/shared'
-
-export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000'),
-}
 
 export default function RootLayout({
   children,
