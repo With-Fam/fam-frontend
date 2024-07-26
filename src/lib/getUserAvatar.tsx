508982437ae625e47ae3c85abc23c4cef1311d71
SeniorDev <tech.eng.me@gmail.com>
2024-07-26 11:57:12 -0400

    I fix feedback

--- src/lib/getUserAvatar.tsx
+++ src/lib/getUserAvatar.tsx
@@ -1,11 +1,8 @@
 import getEnsPfpLink from '@/lib/getEnsPfpLink'
 import { Address } from 'viem'
 
-const getUserAvatar = (avatars: any, address: Address) => {
-  return (
-    getEnsPfpLink(avatars?.ensNames?.[`${address}`]) ||
-    avatars?.openSeaProfileImages?.[`${address}`]
-  )
-}
+const getUserAvatar = (avatars: any, address: Address) =>
+  getEnsPfpLink(avatars?.ensNames?.[`${address}`]) ||
+  avatars?.openSeaProfileImages?.[`${address}`]
 
 export default getUserAvatar
