bf57fddd94d8efdcd7f40b68d3cc93f92db11d64
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-14 17:10:00 -0600

    I remove dev comments.

--- src/lib/split/createSplits.ts
+++ src/lib/split/createSplits.ts
@@ -2,8 +2,6 @@ import { Address } from 'viem'
 import { getEqualSplitParams } from './getEqualSplitParams'
 import { getDeterministicAddress } from './getDeterministicAddress'
 import { getCreateSplitCallData } from './getCreateSplitCallData'
-import { PUSH_SPLIT_FACTORY } from '@/constants/addresses'
-import { CHAIN_ID } from '@/constants/defaultChains'
 
 export interface SplitCreationResult {
   hostsSplitCalldata: `0x${string}`
@@ -20,13 +18,10 @@ export const createSplits = async ({
   partyAddress: Address
   ownerAddress: Address
 }): Promise<SplitCreationResult> => {
-  // Create equal split for hosts
   const hostsSplitParams = getEqualSplitParams(founderAddresses)
   const hostsSplitAddress = await getDeterministicAddress({
     splitParams: hostsSplitParams,
   })
-
-  // Create 50/50 split between party and hosts split
   const partyAndHostsSplitParams = getEqualSplitParams([
     partyAddress,
     hostsSplitAddress,
@@ -34,18 +29,14 @@ export const createSplits = async ({
   const partyAndHostsSplitAddress = await getDeterministicAddress({
     splitParams: partyAndHostsSplitParams,
   })
-
-  // Create calldata for both splits
   const hostsSplitCalldata = getCreateSplitCallData({
     splitParams: hostsSplitParams,
     creator: ownerAddress,
   })
-
   const partyAndHostsSplitCalldata = getCreateSplitCallData({
     splitParams: partyAndHostsSplitParams,
     creator: ownerAddress,
   })
-
   return {
     hostsSplitCalldata,
     partyAndHostsSplitCalldata,
